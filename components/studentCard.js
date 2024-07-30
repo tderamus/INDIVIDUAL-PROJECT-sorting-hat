14176a6372c461df12c88489561b3645dec9a425
tderamus <deramust@gmail.com>
2024-07-29 21:24:51 -0500

    updated setup files to add student houses to HTML, and add student data file structures to the project

---
+++ components/studentCard.js
@@ -0,0 +1,6 @@
+import { studentData } from "../data/studentData";
+
+export const studentCard = (obj) => {
+  return `
+  <div>`
+}
---
+++ scripts/main.js
@@ -0,0 +1,2 @@
+import { renderCards } from "../utils/renderCards.js";
+import { studentData } from "../data/studentData.js";
