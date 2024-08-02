0cd2b9394ba97a908574595588e7dcf56ebb93e4
tderamus <deramust@gmail.com>
2024-08-01 23:09:28 -0500

    added expelled student cards to DOM. Styled sorting form on main.css file. Modified student card stylng on main.css file

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,14 @@ import { studentCard } from "../components/studentCard.js";
 import { expelledStudentData } from "../data/expelledStudentData.js";
 import { expelledStudentCard } from "../components/expelledStudentCard.js";
 
+//Add studentCard to studentData
 
+//add event listener to addStudent form submit button
+
+
+
+
+//Start the sorting application
 const startApp = () => {
   studentCard(studentData)
   expelledStudentCard(expelledStudentData)
